fc40f4b673b24ab1d7632299af0b590e90fc48d1
J.D <r.w.rollet@gmail.com>
2018-07-09 22:43:52 +0200

    fix ui test

--- src/test/java/com/spring/controller/userInterfaceTests/SimpleUserInterfaceTest.java
+++ src/test/java/com/spring/controller/userInterfaceTests/SimpleUserInterfaceTest.java
@@ -2,6 +2,9 @@ package com.spring.controller.userInterfaceTests;
 
 import static org.junit.Assert.assertTrue;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,24 +15,38 @@ import org.testng.annotations.Test;
 
 public class SimpleUserInterfaceTest {		
 	    private WebDriver driver;
-	    private String TEST_URL = "http://localhost:8090/";
+	    private String BASE_URL = "http://localhost:";
+	    private String HOST_PORT = "8090";
+	    private String VM_PORT = "9090";
+	    private String TEST_URL = "/test/mywebapp/login";
+	    
+	    private String HOST_URL = BASE_URL + HOST_PORT + TEST_URL;
+	    private String VM_URL = BASE_URL + VM_PORT + TEST_URL;
+	    
 	    private String USERNAME = "devops";
 	    private String PASSWORD = "devops123";
 	    
 		@Test				
-		public void testLogin() {	
-			driver.get(TEST_URL);  
+		public void testLogin() throws UnknownHostException {	
+			String hostname = InetAddress.getLocalHost().getHostName();
 			
-			WebElement name = driver.findElement(By.name("userName"));
-			WebElement password = driver.findElement(By.name("password"));			
-			WebElement submit = driver.findElement(By.name("submit"));
+			if ("ci-server".equals(hostname)) {
+				driver.get(VM_URL);  
+			}
+			else {
+				driver.get(HOST_URL);
+			}
 			
+			WebElement name = driver.findElement(By.cssSelector("[name=\"userName\"]"));
+			WebElement password = driver.findElement(By.cssSelector("[name=\"password\"]"));			
+			WebElement submit = driver.findElement(By.cssSelector("[type=\"submit\"]"));
+
 			name.sendKeys(USERNAME);
 			password.sendKeys(PASSWORD);
 			submit.submit();
 			
 			String title = driver.getTitle();				 
-			assertTrue("Login test failed", title.contains("Success Form")); 		
+			assertTrue("Could not access to application", title.contains("Success Form")); 			
 		}	
 		
 		@BeforeTest
